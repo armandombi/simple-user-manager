2f82b3f6b723ab61616c9cb318adbadae0427739
Armando Bracho <armandombi@gmail.com>
2015-08-16 18:13:10 -0430

    Initial commit

---
+++ wp-sum.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Simple User Manager
+Plugin URI:
+Description: This plugin will let you mange your users login, registration, profile edition and password management all in a simple yet elegant solution.
+Version: 1.0.0
+Author: Armando Bracho, Daniel Bracho
+Author URI:
+License: GPL2
+*/
+
+
+
